7fb435b90589f2009246b50deb67a90b174a2e1f
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2021-09-20 19:58:38 +0200

    Changed Lock style

--- MetaModServer/Services/ServiceTransactions.cs
+++ MetaModServer/Services/ServiceTransactions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace MetaModFramework.Services
@@ -8,23 +7,24 @@ namespace MetaModFramework.Services
     public class ServiceTransactions
     {
         private long _lock;
-        public bool Lock 
+        public bool Lock
         {
-            [MethodImpl(MethodImplOptions.Synchronized)]
             get
             {
-                /* Interlocked.Read() is only available for int64,
-                 * so we have to represent the bool as a long with 0's and 1's
-                 */
-                return Interlocked.Read(ref this._lock) == 1;
+                lock (SyncLock)
+                {
+                    return Interlocked.Read(ref this._lock) == 1;
+                }
             }
-            [MethodImpl(MethodImplOptions.Synchronized)]
             set
             {
-                Interlocked.Exchange(ref this._lock, Convert.ToInt64(value));
+                lock (SyncLock)
+                {
+                    Interlocked.Exchange(ref this._lock, Convert.ToInt64(value));
+                }
             }
         }
-        
+
         private static readonly object SyncLock = new ();
 
         private static readonly Dictionary<string, ServiceTransactions> Directory = new();
