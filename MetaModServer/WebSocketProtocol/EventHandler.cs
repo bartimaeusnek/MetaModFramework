779aa72d8694f222ca1c4d9c2a27040cb84c7225
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2021-11-20 18:13:13 +0100

    WebSocket Implementation (#1)

---
+++ MetaModServer/WebSocketProtocol/EventHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace MetaModFramework.WebSocketProtocol
+{
+    public static class WsEventHandler
+    {
+        private static readonly object SyncLock = new ();
+        private static          bool   _needsSyncBackingField;
+        public static bool NeedsSync
+        {
+            get
+            {
+                lock (SyncLock)
+                {
+                    return _needsSyncBackingField;
+                }
+            }
+            set
+            {
+                lock (SyncLock)
+                {
+                    _needsSyncBackingField = value;
+                }
+            }
+        }
+
+        public static void OnHandler()
+        {
+            NeedsSync = true;
+        }
+    }
+}
